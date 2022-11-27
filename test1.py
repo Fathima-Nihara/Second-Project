12f6d0119b7494345ac17164c76e3aeb55d0e8ee
Fathima Nihara <118329291+Fathima-Nihara@users.noreply.github.com>
2022-11-27 07:29:21 +0530

    change input value

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
-p1=int(input('p1:'))
-p2=int(input('p2:'))
-p3=int(input('p3:'))
-print(p1+p2+p3)
-n= p1+p2+p3
+p1=int(input('1:'))
+p2=int(input('2:'))
+p3=int(input('3:'))
+print(1+2+3)
+n= 1+2+3
 print(n)
 
