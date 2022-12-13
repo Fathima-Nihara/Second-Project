b4da387f028f0faa4c5ea2d1d4be5198b6081205
Fathima Nihara <nihafa99@gmail.com>
2022-12-13 11:12:23 +0530

    define n2 and n3

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 n1=int(input('n1:'))
-n1=int(input('n2:'))
-n1=int(input('n3:'))
+n2=int(input('n2:'))
+n3=int(input('n3:'))
 t=n1+n2
+print(t,'hi')
+print(t,'hi')
 print(t,'hi')
 print ('hello')
 print(n1 * n2)
